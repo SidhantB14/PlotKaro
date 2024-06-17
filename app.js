1831f593652cb2a98f167c414f88127fd256fe0a
Sidhant Barsinge <barsinge_kd24@iift.edu>
2024-06-18 00:11:53 +0530

    second commit

--- app.js
+++ app.js
@@ -110,9 +110,9 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
     res.status(statusCode).render('error', { err })
 })
-const port = process.env.PORT||3000 
-app.listen(port, () => {
-    console.log(`Serving on port ${port}`)
+
+app.listen(10000, () => {
+    console.log("Serving on 300")
 })
 
 
